1b7f9684f2b3349ee643cfa54978e484fd0cf3ff
Adriano <adrianoferrantedepiante48@gmail.com>
2022-12-26 12:52:13 -0500

    added a functional react display that formats the csv files to columns:values separating each row from each other, also added download TXT file functionality

--- src/index.js
+++ src/index.js
@@ -7,61 +7,64 @@ import Papa from 'papaparse';
 
 function CSVReader() {
     const [data, setData] = useState([]);
-  
+
     const handleFileSelect = (event) => {
-      const file = event.target.files[0];
-      const reader = new FileReader();
-  
-      reader.onload = (event) => {
-        const csvData = Papa.parse(event.target.result, {
-          header: true,
-          dynamicTyping: true,
-        });
-        setData(csvData.data);
-      };
-  
-      reader.readAsText(file);
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = (event) => {
+            const csvData = Papa.parse(event.target.result, {
+                header: true,
+                dynamicTyping: true,
+            });
+            setData(csvData.data);
+        };
+        reader.readAsText(file);
     };
-  
+
     const handleDownload = () => {
-      const csv = Papa.unparse(data);
-      const blob = new Blob([csv], { type: 'text/plain;charset=utf-8;' });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.style.display = 'none';
-      a.href = url;
-      a.download = 'data.txt';
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
+        const csv = Papa.unparse(data);
+        const blob = new Blob([csv], { type: 'text/plain;charset=utf-8;' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = 'data.txt';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
     };
-  
+
+
     return (
-      <div>
-        <input type="file" onChange={handleFileSelect} />
-        <button onClick={handleDownload}>Download</button>
-        <table>
-          <thead>
-            <tr>
-              {data.length > 0 &&
-                Object.keys(data[0]).map((key) => (
-                  <th key={key}>{key}</th>
-                ))}
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((row) => (
-              <tr key={row.id}>
-                {Object.values(row).map((value) => (
-                  <td key={value}>{value}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+        <div>
+            <input type="file" onChange={handleFileSelect} />
+            <button onClick={handleDownload}>Download</button>
+
+
+
+            <div>
+                <div>
+                    {data.map((item, index) => (
+                        <div key={index} style={{ border: '1px solid black' }}>
+
+                            {Object.keys(item).map((keyItems, i) => {
+                                if (Object.values(item)[i]) { 
+                                    return (
+                                        <li key={index}>{JSON.stringify(Object.keys(item)[i])} : {JSON.stringify(Object.values(item)[i])}</li>
+                                    )
+                             }
+
+
+                            })}
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     );
-  }
-  
+}
+
 
 ReactDOM.render(<CSVReader />, document.getElementById('root'));
