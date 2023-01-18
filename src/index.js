aa53f6976d6723eb9a712f48fedddab3a9727581
Adriano <adrianoferrantedepiante48@gmail.com>
2023-01-18 17:37:49 -0500

    added condifional display on upload component

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ function CSVReader() {
         event.preventDefault()
     }
 
-         
+
     const handleDrop = (event) => {
         event.preventDefault()
         handleFileSelect(event)
@@ -47,24 +47,57 @@ function CSVReader() {
         a.click();
         document.body.removeChild(a);
     };
- 
+
+
+
+
     return (
-        <div class='text-center'>
-            <div class='container'>
+        <div class='text-center' style={{ height: "100%" }}>
+            <div class='w-100 h-100 p-3'>
                 <div class="bg-light p-5 rounded">
-                    {(!data.length[1]) && (
+                    {!data[1] ? <h1><div className='btn btn-lg btn-primary'
+                        onDragOver={handleDragOVer}
+                        onDrop={handleDrop}
+                    >
+                        <h1>Drop Files Here</h1>
+                        <h1>Or</h1>
+                        <input type="file" onChange={handleFileSelect} />
+                    </div></h1> : <div>
+                        {data.map((item, index) => (
+                            <div key={index} style={{ border: '1px solid black' }}>
+                                {Object.keys(item).map((keyItems, i) => {
+                                    if (Object.values(item)[i]) {
+                                        return (
+                                            <li key={index}>{JSON.stringify(Object.keys(item)[i]).replaceAll('"', "")} : {JSON.stringify(Object.values(item)[i]).replaceAll('"', "")}</li>
+                                        )
+                                    }
+                                })}
+                            </div>
+                        ))}
+                    </div>
+
+                    }
+
+
+                    {/* (!data.length[1]) && 
                             <div className='btn btn-lg btn-primary'
                             onDragOver={handleDragOVer}
                             onDrop={handleDrop}
                             >
                                 <h1>Drop Files Here</h1>
                                 <h1>Or</h1>
-                                <input type="file" onChange={handleFileSelect}/>
-                        </div>)
-                    }
-                    </div>
+                                <input type="file" onChange={handleFileSelect}/>                           
+                        </div> */}
+
+
+
+
                 </div>
-            </div>);
+
+            </div>
+            <div>
+            </div>
+        </div>);
 }
 
 
@@ -73,21 +106,21 @@ ReactDOM.render(<CSVReader />, document.getElementById('root'));
 
 
 // <button onClick={handleDownload}>Download</button>
-            // <div>
-            //     <div>
-            //         {data.map((item, index) => (
-            //             <div key={index} style={{ border: '1px solid black' }}>
-            //                 {Object.keys(item).map((keyItems, i) => {
-            //                     if (Object.values(item)[i]) { 
-            //                         return (
-            //                             <li key={index}>{JSON.stringify(Object.keys(item)[i])} : {JSON.stringify(Object.values(item)[i])}</li>
-            //                         )
-            //                  }
-            //                 })}
-            //             </div>
-            //         ))}
-            //     </div>
-            // </div>
+{/* <div>
+    <div>
+        {data.map((item, index) => (
+            <div key={index} style={{ border: '1px solid black' }}>
+                {Object.keys(item).map((keyItems, i) => {
+                    if (Object.values(item)[i]) {
+                        return (
+                            <li key={index}>{JSON.stringify(Object.keys(item)[i])} : {JSON.stringify(Object.values(item)[i])}</li>
+                        )
+                    }
+                })}
+            </div>
+        ))}
+    </div>
+</div> */}
 
 
 
